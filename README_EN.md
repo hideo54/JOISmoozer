cae42ec68f5992170d67c82cf59edee2d7f22227
Hideo Yasumoto <hideo54.me@gmail.com>
2015-06-07 21:25:49 +0900

    Added English README and included some fixes

---
+++ README_EN.md
@@ -0,0 +1,33 @@
+#JOISmoozer
+
+##About
+In attending JOI, making many output file will bother you.
+This program automatically executes a program from given input file, and makes output files.
+Let's do our best together.
+
+##Version
+Ver 1.1.1
+
+##Operating Environment
+* Python 2.6 or later
+
+main3.py is put for Python 3 users.
+
+###Confirmed Operating Environment
+main.py: Python 2.7.6 on OS X 10.10.1 (2014/12/14)
+
+main3.py: Unconfirmed
+
+##How to use
+1. Change line 5-8 as you like.
+Format the problem number as {prb}, and input filename as {num}.
+2. If you wanna use a language neither C nor Python, add a setting under line 19 as you like by using 'elif'. (and fork me on GitHub!)
+3. Execute this program by Python. Input a problem number, and if no error appears, congrats, it worked successfully!
+If a error happened, it'd be caused by either your setting or executed file. Judge from the error.
+
+##Contact me
+Twitter… [@hideo54](https://www.twitter.com/hideo54)
+
+Email… hideo54.pub@gmail.com
+
+Contact me if there's a bug.
--- main3.py
+++ main3.py
@@ -1,24 +1,24 @@
 import os
 
-#The following is the default settings.
+#The following is the default setting.
 #If you need, rewrite as you want.
-language = 'Python' #<-Rewrite the language name you wanna use.
+language = 'Python' #<-Rewrite language you wanna use.
 file_format = '~/Desktop/JOI{prb}.py'
 input_format = '~/Downloads/2014-yo-t{prb}-in{num}.txt'
 output_format = 'anser{prb}-{num}.txt'
 
 #Main program
-problem = input("What's the number of the problem you're solving now? :")
+problem = input("The problem number you're solving:")
 file = file_format.format(prb=problem)
 
-#I prepared two language, C and Python.
-#If you rewrote line 5, add command using 'elif'.
+#I prepared two languages, C and Python.
+#If you rewrote line 5, add a command by using 'elif'.
 if language == 'C':
     command_format = 'gcc {f}; ./a.out'
 elif language == 'Python':
     command_format = 'python {f}'
 else:
-    command_format = input('What command you wanna use? Input the command writing {f} as filename')
+    command_format = input('What command do you wanna use? Input the command by using {f} as filename')
 
 try:
     for number in range(5):
@@ -30,4 +30,4 @@ try:
         os.system('{cmd} < {IN} > {out}'.format(cmd=command, f=file, IN=inpt, out=output))
     print('Successfully done')
 except:
-    print('Error! Check your settings; line 5-9')
+    print('Error! Check your setting (line 5-9)')
